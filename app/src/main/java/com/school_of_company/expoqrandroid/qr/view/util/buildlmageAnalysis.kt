6577532adb9b24f091722abaa1a7f96820a3980f
kimjeakwan <s24042@gsm.hs.kr>
2025-04-01 16:10:15 +0900

    :sparkles: ::  buildlmageAnalysis 추가

---
+++ app/src/main/java/com/school_of_company/expoqrandroid/qr/view/util/buildlmageAnalysis.kt
@@ -0,0 +1,17 @@
+package com.school_of_company.expoqrandroid.qr.view.util
+
+import androidx.camera.core.ImageAnalysis
+import androidx.core.content.ContextCompat
+/**
+ * QR 코드 분석을 위한 ImageAnalysis 객체를 생성하는 함수
+ **/
+internal fun buildImageAnalysis(
+    context: android.content.Context,
+    onQrcodeScanned: (String?) -> Unit
+): ImageAnalysis {
+    return ImageAnalysis.Builder()
+        .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+        .build().apply {
+            setAnalyzer(ContextCompat.getMainExecutor(context), QrcodeAnalyzer(onQrcodeScanned))
+        }
+}
