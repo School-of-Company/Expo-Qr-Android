ea99a8b4dde7a961b4f0f7c008e6268d3a11b398
kimjeakwan <s24042@gsm.hs.kr>
2025-04-02 14:15:48 +0900

    :recycle: :: 모든 글시체 적용

--- app/src/main/java/com/school_of_company/expoqrandroid/ui/theme/Type.kt
+++ app/src/main/java/com/school_of_company/expoqrandroid/ui/theme/Type.kt
@@ -10,8 +10,14 @@ import com.school_of_company.expoqrandroid.R
 
 
 val pretendard = FontFamily(
-    Font(R.font.pretendard_bold, FontWeight.Bold)
-)
+    Font(R.font.pretendard_bold, FontWeight.Bold),
+    Font(R.font.pretendard_extralight, FontWeight.ExtraLight),
+    Font(R.font.pretendard_light, FontWeight.Light),
+    Font(R.font.pretendard_medium, FontWeight.Medium),
+    Font(R.font.pretendard_regular, FontWeight.Normal),
+    Font(R.font.pretendard_semibold, FontWeight.SemiBold),
+    Font(R.font.pretendard_thin, FontWeight.Thin),
+    )
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
