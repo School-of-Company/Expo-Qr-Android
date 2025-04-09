c01c70f9adeb0bd8c3f92772ffb291cca2a8de02
jsw9436 <jswjsw101010@gmail.com>
2025-04-10 01:24:07 +0900

    :recycle: :: Hilt삭제 및 BASE_URL 분리

--- app/src/main/java/com/school_of_company/expoqrandroid/network/NetworkModule.kt
+++ app/src/main/java/com/school_of_company/expoqrandroid/network/NetworkModule.kt
@@ -1,53 +1,39 @@
-package com.school_of_company.expoqrandroid.di
+package com.school_of_company.expoqrandroid.network
 
 import android.util.Log
+import com.school_of_company.expoqrandroid.BuildConfig
 import com.squareup.moshi.Moshi
-import dagger.Module
-import dagger.Provides
-import dagger.hilt.InstallIn
-import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import java.util.concurrent.TimeUnit
-import javax.inject.Singleton
 
-@Module
-@InstallIn(SingletonComponent::class)
 object NetworkModule {
 
-    private const val BASE_URL = "https://qr-api-url.com/" // 나중에 변경
-
-    @Provides
-    @Singleton
-    fun provideLoggingInterceptor(): HttpLoggingInterceptor =
-        HttpLoggingInterceptor { message -> Log.d("HttpLog", message) }
+    private val loggingInterceptor by lazy {
+        HttpLoggingInterceptor { message -> Log.d("HTTP_LOG", message) }
             .apply { level = HttpLoggingInterceptor.Level.BODY }
+    }
 
-    @Provides
-    @Singleton
-    fun provideOkHttpClient(
-        loggingInterceptor: HttpLoggingInterceptor
-    ): OkHttpClient = OkHttpClient.Builder()
-        .connectTimeout(30, TimeUnit.SECONDS)
-        .readTimeout(30, TimeUnit.SECONDS)
-        .writeTimeout(30, TimeUnit.SECONDS)
-        .addInterceptor(loggingInterceptor)
-        .build()
+    val okHttpClient: OkHttpClient by lazy {
+        OkHttpClient.Builder()
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS)
+            .addInterceptor(loggingInterceptor)
+            .build()
+    }
 
-    @Provides
-    @Singleton
-    fun provideMoshi(): Moshi = Moshi.Builder().build()
+    val moshi: Moshi by lazy {
+        Moshi.Builder().build()
+    }
 
-    @Provides
-    @Singleton
-    fun provideRetrofit(
-        okHttpClient: OkHttpClient,
-        moshi: Moshi
-    ): Retrofit = Retrofit.Builder()
-        .baseUrl(BASE_URL)
-        .client(okHttpClient)
-        .addConverterFactory(MoshiConverterFactory.create(moshi))
-        .build()
+    val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BuildConfig.QR_BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+    }
 }
