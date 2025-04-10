512c51283b0e0333c0f795b52b247c3d765f4b79
jsw9436 <jswjsw101010@gmail.com>
2025-04-10 11:36:52 +0900

    :recycle: :: moshi 의존성 제거

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -85,8 +85,7 @@ dependencies {
 
     // Network
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
-    implementation("com.squareup.moshi:moshi:1.15.1")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.11")
 
     debugImplementation("com.readystatesoftware.chuck:library:1.1.0")
