b81c29e72736b3f4009dae1c7c48bc794e7bf33b
kimjeakwan <s24042@gsm.hs.kr>
2025-05-01 13:16:32 +0900

    :recycle: :: type 새로운 글시체 bodyMedium 추가

--- app/src/main/java/com/school_of_company/expoqrandroid/ui/theme/Type.kt
+++ app/src/main/java/com/school_of_company/expoqrandroid/ui/theme/Type.kt
@@ -22,11 +22,19 @@ val pretendard = FontFamily(
 val Typography = Typography(
     bodyLarge = TextStyle(
         fontFamily = pretendard,
-        fontWeight = FontWeight.Normal,
+        fontWeight = FontWeight.Bold,
         fontSize = 24.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = pretendard,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        lineHeight = 16.8.sp
     )
+
+
     /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
