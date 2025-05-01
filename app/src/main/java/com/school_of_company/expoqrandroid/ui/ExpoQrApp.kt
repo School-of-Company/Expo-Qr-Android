4f24adeb5044fb68a6e0c8b852e047159c2dcb27
kimjeakwan <s24042@gsm.hs.kr>
2025-05-01 17:25:45 +0900

    :sparkles: :: ExpoQrApp 추가

---
+++ app/src/main/java/com/school_of_company/expoqrandroid/ui/ExpoQrApp.kt
@@ -0,0 +1,29 @@
+package com.school_of_company.expoqrandroid.ui
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navigation
+import com.school_of_company.expoqrandroid.qr.view.QrScannerScreen
+
+
+@Composable
+fun ExpoQrApp() {
+    val navController = rememberNavController()
+
+
+    NavHost(
+        navController = navController,
+        startDestination = "app"
+    ) {
+        navigation(
+            route = "app",
+            startDestination = "qrScreen"
+        ) {
+            composable("qrScreen") {
+                QrScannerScreen()
+            }
+        }
+    }
+}
