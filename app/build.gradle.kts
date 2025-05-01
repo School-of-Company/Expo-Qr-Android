d4228c92689f1c13e91bfebf9a911f1fe3558412
kimjeakwan <s24042@gsm.hs.kr>
2025-05-01 13:53:07 +0900

    :heavy_minus_sign: :: 쓰지 않는 Chuck 라이브러리 삭제

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -88,9 +88,6 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.11")
 
-    debugImplementation("com.readystatesoftware.chuck:library:1.1.0")
-    releaseImplementation("com.readystatesoftware.chuck:library-no-op:1.1.0")
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
